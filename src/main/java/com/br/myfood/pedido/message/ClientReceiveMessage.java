89a871039b4e2d61d6a2b57a30e2e04595c82cfb
João Marcos Rodrigues Nascimento <joao.marcos@gipi.local>
2020-10-16 18:02:28 -0300

    AAAA

--- src/main/java/com/br/myfood/pedido/message/ClientReceiveMessage.java
+++ src/main/java/com/br/myfood/pedido/message/ClientReceiveMessage.java
@@ -5,14 +5,23 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.br.myfood.pedido.dto.ClienteOrderDto;
+import com.br.myfood.pedido.entity.ClienteOrder;
+import com.br.myfood.pedido.repository.ClienteOrderRepository;
 
 @Component
 public class ClientReceiveMessage {
 	
+	private final ClienteOrderRepository clienteOrderRepository;
+	
+	public ClientReceiveMessage(ClienteOrderRepository clienteOrderRepository) {
+		this.clienteOrderRepository = clienteOrderRepository;
+	}
+
+
 	@RabbitListener(queues = {"${cadastro.client.rabbitmq.queu}"})
 	public void receiveMessage(@Payload ClienteOrderDto clientOrderDto) {
 		
-		System.out.println(clientOrderDto);
+		clienteOrderRepository.save(new ClienteOrder(null, clientOrderDto.getIdCliente()));
 		
 	}
 
